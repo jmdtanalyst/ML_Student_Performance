a9fbe19eba17004230254b3bfdd3b069e750f62d
jmdtanalyst <2023422@student.cct.ie>
2024-01-09 00:04:10 +0000

    Creating EDA And Model Jupyter Notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from logger import logging
 
 
 # Function error_message_detail, will raise a Custom error message with  file name, line and error.
@@ -21,6 +21,15 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+    
+# code to test the logging:
+'''
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Error divide by zero")
+        raise CustomException(e,sys)
 
 
-        
+'''
